81a196591fe992e3b627d6c17c52842993d00718
nSakkriou <nathansakkriou@gmail.com>
2024-01-10 16:10:02 +0100

    update

--- start.sh
+++
@@ -1,4 +0,0 @@
-python3 /home/nsakkriou/github-profile-update/nSakkriou/main.py >> /cron.log
-git -C /home/nsakkriou/github-profile-update/nSakkriou/ add . >> /cron.log
-git -C /home/nsakkriou/github-profile-update/nSakkriou/ commit -m "update" >> /cron.log
-git -C /home/nsakkriou/github-profile-update/nSakkriou/ push origin main >> /cron.log
